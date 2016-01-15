fe7b761a4deed7d1a326bdae7f3697a52be6ebd8
BadworkerN <381900623@qq.com>
2016-01-15 07:43:37 -0800

    basis

---
+++ 2016-1-15/hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author:Badboy
+	> Mail:381900623@qq.com 
+	> Created Time: Fri 15 Jan 2016 07:42:05 AM PST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout<<"hello"<<endl;
+	return 0;
+}
